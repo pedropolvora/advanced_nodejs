ba7714970ecd96521f8aeae36633946079f52eb7
Pedro Polvora <pedro.polvora@gmail.com>
2017-03-13 11:02:41 -0500

    event_loop script with more sync funcs

--- scripts/event_loop.js
+++ scripts/event_loop.js
@@ -1,13 +1,16 @@
 console.log('Starting...');
 
-function slowFunction(exponent){
+function slowFunction(exponent, msg){
 	var top_limit = Math.pow(10, exponent);
 	console.log('running a slow function for', top_limit);
 	for(var i = 0; i < top_limit; i++){
-		if(i === Math.round(top_limit/2))console.log(' half !');
+		if(i === Math.round(top_limit/2))console.log(`half way.. ${msg}`);
 	}
 }
 
-process.nextTick(slowFunction,9);
-process.nextTick(slowFunction,4);
+process.nextTick(slowFunction,9,1);
+process.nextTick(slowFunction,4,2);
 console.log('hello');
+slowFunction(9,3);
+slowFunction(9,4);
+slowFunction(9,5);
