c5e3ea2591c3f08a7269545d78b33867e6a22a11
Pedro Polvora <pedro.polvora@gmail.com>
2017-04-05 15:51:00 +0100

    improve cluster message code

--- scripts/child_processes/cluster_module/clusterMessages.js
+++ scripts/child_processes/cluster_module/clusterMessages.js
@@ -1,9 +1,13 @@
 const cluster = require('cluster');
 const os = require('os');
 
-
-
-
+// *** MOCK DB Call 
+const numberOfUsersInDB = function() {
+	this.count = this.count || 5;
+	this.count = this.count * this.count;
+	return this.count;
+}
+// ***
 
 if (cluster.isMaster) {
 	const cpus = os.cpus().length;
@@ -15,9 +19,22 @@ if (cluster.isMaster) {
 	// the following console shows the object that contains all the worker
 	console.dir(cluster.workers, { depth: 0 });
 
+	// sending messages to the workers
 	Object.values(cluster.workers).forEach(worker => {
 		worker.send(`Hello Worker ${worker.id}`);
 	});
+
+	const updateWorkers = () =>{
+		const usersCount = numberOfUsersInDB();
+		Object.values(cluster.workers).forEach( worker => {
+			worker.send({ usersCount });
+		});
+	};
+
+	// periodically updating workers
+	updateWorkers();
+	setInterval(updateWorkers, 10000);
+
 } else {
 	require('./serverMessage.js');
 }
